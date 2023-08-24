bc2f7be2fd5f7aa963cf282d14cd59155738f03d
1StepBehind <poweredbypaintball@gmail.com>
2023-08-24 17:56:22 +0930

    small changes

--- index.js
+++ index.js
@@ -2,8 +2,47 @@ function getComputerChoice() {
 
 }
 
+function getPlayerChoice(){
+    prompt ("rock,paper,scissors","")
+}
+// prompt from user
+
 function playRound(playerSelection, computerSelection) {
 
 }
+
+function game(){
+
+    // keep score of wins + losses
+    // loop functions??
+}
+
+// play 5 rounds 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // "You Lose! Paper beats Rock"
+// "You Lose! Rock beats Scissors"
+// "You Lose! Scissors beats Paper"
+// "You Win! Paper beats Rock"
+// "You Win! Rock beats Scissors"
+// "You Win! Scissors beats Paper"
 // refer back to problem solving model
+
